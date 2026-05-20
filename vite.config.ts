d3acad11b56e365bf7cab01674b31b37d517a955
vinayan00123 <vinayanbalasubramanian@gmail.com>
2026-05-20 12:03:25 +0530

    chore: update deployment homepage and set base path for GitHub Pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({mode}) => {
   return {
+    base: '/My-Portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
